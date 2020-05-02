f7ab22841d0217fa2ea63c3688e01f1e7626c1a2
Dan Pham <phamdan.sj@gmail.com>
2020-05-02 14:37:42 -0700

    Initial Commit

---
+++ Udacity Coffee Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udacity Coffee Shop
+//
+//  Created by Dan Pham on 5/2/20.
+//  Copyright © 2020 Dan Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
